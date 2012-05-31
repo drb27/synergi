4c29a7caae7b32d6e4e40d9c44dcc3e4cd5c9226
David Bradshaw <anon@anon.com>
2012-05-31 11:25:44 -0400

    Added types.h

---
+++ types.h
@@ -0,0 +1,22 @@
+/*
+ * types.h
+ *
+ *  Created on: May 31, 2012
+ *      Author: drb
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <stdint.h>
+
+#define NULL (0)
+
+
+
+namespace synergi
+{
+}
+
+
+#endif /* TYPES_H_ */
