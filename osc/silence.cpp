bcd41c7c3da6879dfd7fc0829970e4945a441d7c
David Bradshaw <anon@anon.com>
2012-06-01 14:40:19 -0400

    Added a silence oscillator

---
+++ osc/silence.cpp
@@ -0,0 +1,23 @@
+/*
+ * silence.cpp
+ *
+ *  Created on: Jun 1, 2012
+ *      Author: drb
+ */
+
+#include <common/ns.h>
+#include <common/types.h>
+#include "silence.h"
+
+s_namespace_2(synergi,engine)
+
+silence::silence() {
+	// TODO Auto-generated constructor stub
+
+}
+
+silence::~silence() {
+	// TODO Auto-generated destructor stub
+}
+
+s_namespace_end_2
---
+++ osc/silence.h
@@ -0,0 +1,25 @@
+/*
+ * silence.h
+ *
+ *  Created on: Jun 1, 2012
+ *      Author: drb
+ */
+
+#ifndef SILENCE_H_
+#define SILENCE_H_
+
+#include <common/ns.h>
+#include <common/types.h>
+#include "oscillator.h"
+
+s_namespace_2(synergi,engine)
+
+class silence : public oscillator {
+public:
+	silence();
+	virtual ~silence();
+};
+
+s_namespace_end_2
+
+#endif /* SILENCE_H_ */
