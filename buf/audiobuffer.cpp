30c0c8792a208493f1fd2bc1df17cadc1b811f2a
David Bradshaw <drb@drbmac-vmlnx.(none)>
2012-06-01 07:58:48 -0400

    Added the missing buf subdir

---
+++ buf/audiobuffer.cpp
@@ -0,0 +1,22 @@
+/*
+ * audiobuffer.cpp
+ *
+ *  Created on: May 31, 2012
+ *      Author: drb
+ */
+
+#include "ns.h"
+#include "audiobuffer.h"
+
+s_namespace_2(synergi,engine)
+
+audiobuffer::audiobuffer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+audiobuffer::~audiobuffer() {
+	// TODO Auto-generated destructor stub
+}
+
+s_namespace_end_2
---
+++ buf/audiobuffer.h
@@ -0,0 +1,21 @@
+/*
+ * audiobuffer.h
+ *
+ *  Created on: May 31, 2012
+ *      Author: drb
+ */
+
+#ifndef AUDIOBUFFER_H_
+#define AUDIOBUFFER_H_
+
+s_namespace_2(synergi,engine)
+
+class audiobuffer {
+public:
+	audiobuffer();
+	virtual ~audiobuffer();
+};
+
+#endif /* AUDIOBUFFER_H_ */
+
+s_namespace_end_2
